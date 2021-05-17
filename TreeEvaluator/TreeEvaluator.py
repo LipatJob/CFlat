9bf25b4654fdea08b094ac863fd88c1f46cd4707
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-17 18:33:04 +0800

    added TreeEvaluator

---
+++ TreeEvaluator/TreeEvaluator.py
@@ -0,0 +1,7 @@
+from Lib.Node import Node
+from Lib.Token import Token
+
+class TreeEvaluator:
+    def run(self, root: Node):
+        # WRITE CODE FOR TREE EVALUATOR HERE
+        pass
--- __main_.py
+++ __main_.py
@@ -1,16 +1,19 @@
 from LexicalAnalyzer.LexicalAnalyzer import LexicalAnalyzer
 from SemanticAnalyzer.SemanticAnalyzer import  SemanticAnalyzer
 from SyntaxAnalyzer.SyntaxAnalyzer import SyntaxAnalyzer
+from TreeEvaluator.TreeEvaluator import TreeEvaluator
 
 def __main__():
     lexer = LexicalAnalyzer()
     parser = SyntaxAnalyzer()
     semanticAnalyzer = SemanticAnalyzer()
+    evaluator = TreeEvaluator()
 
     file_name = ""
 
     tokens = lexer.run(file_name)
     tree = parser.run(tokens)
     semanticAnalyzer.run(tree)
+    evaluator.run(tree)
 
 __main__()
