2ff264d24451b4a0221bf0756d78f24987de4268
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-19 23:50:22 +0800

    added more node values

--- test_syntax.py
+++ test_syntax.py
@@ -1,6 +1,8 @@
 from SyntaxAnalyzer.SyntaxAnalyzer import SyntaxAnalyzer
 from Lib.Token import *
 from Lib.Token import TokenType as TT
+from Lib.Node import Node, NodeType as NT
+from Li
 
 tokens = [
     Token(TT.MINUS, "-"),
@@ -19,3 +21,9 @@ analyzer = SyntaxAnalyzer()
 root = analyzer.run(tokens)
 print("----------")
 analyzer.print_tree(root)
+
+Node(NT.MULTIPLY, [
+    Node(NT.ADD, [
+        Node(NT.INT_LITERAL, [3]),
+        Node(NT.INT_LITERAL, [4])]),
+    Node(NT.INT_LITERAL, [2])])
