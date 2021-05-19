c9b586e3c074d16fab5336ef709b22029d6c5d4c
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-19 23:52:15 +0800

    added symbols to Node

--- Lib/Node.py
+++ Lib/Node.py
@@ -14,18 +14,43 @@ class NodeType:
     ASSIGNMENT = "ASSIGNMENT" # (DATE_TYPE, IDENTIFER, EXPRESSION)
     PRINT = "PRINT"
     INPUT = "INPUT"
+
     FOR = "FOR"
     WHILE = "WHILE"
+
     IF = "IF"
     ELIF = "ELIF"
     ELSE = "ELSE"
-    NEGATE = "NEGATE"
-    NOT = "NOT"
 
     IDENTIFIER = "IDENTIFIER"
     INT_LITERAL = "INT_LITERAL"
     STRING_LITERAL = "STRING_LITERAL"
     BOOL_LITERAL = "BOOL_LITERAL"
+
+    NEGATE = "NEGATE"
+    ADD = "ADD"
+    SUBTRACT = "SUBTRACT"
+    MULTIPLY = "MULTIPLY"
+    DIVIDE = "DIVIDE"
+
+    EQUAL = "EQUAL"
+    AND = "AND"
+    OR = "OR"
+    NOT = "NOT"
+
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    STAR = "STAR"
+    SLASH = "SLASH"
+
+    DOUBLE_EQUAL = "DOUBLE_EQUAL"
+    NOT_EQUAL = "NOT_EQUAL"
+    MORE_THAN = "MORE_THAN"
+    LESS = "LESS"
+    MORE = "MORE"
+    LESS_EQUAL = "LESS_EQUAL"
+    MORE_EQUAL = "MORE_EQUAL"
+    END_OF_FILE = "END_OF_FILE"
 
     @classmethod
     def to_node_type(cls, token_type):
