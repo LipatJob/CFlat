d60cacbb47c096527e41fe3cd19abbd74bb91d70
charmaine <charmaine.rabano@gmail.com>
2021-05-23 22:06:32 +0800

    incomplete evaluator

--- TreeEvaluator/TreeEvaluator.py
+++ TreeEvaluator/TreeEvaluator.py
@@ -1,20 +1,122 @@
-from Lib.Node import Node
+from Lib.Node import Node, NodeType as NT, ExpressionType as ET
 from Lib.Token import Token
 
+class SymbolTable:
+    table = []
+
+    def bind(self, identifier, value, data_type):
+        self.table.append([identifier, value, data_type])
+
+    def lookup(self, identifier):
+        for item in self.table:
+            if item[0] == identifier:
+                return item[1]
+        else:
+            return False
+
 class TreeEvaluator:
-    def run(self, root: Node, symbol_table = None):
+    symbol_table = SymbolTable()
+
+    def run(self, root: Node):
         if root:
-            self.traverse(root)
+            program = self.evaluate(root)
+            exec(program)
 
         return
     
-    def traverse(self, node):
-        print()
-        print(node.value)
+    def evaluate(self, node):
+        if node.value == NT.PROGRAM:
+            program = ""
 
-        if node.expression_type:
-            print(node.expression_type)
-
-        if node.parameters:
             for child in node.parameters:
-                self.traverse(child)
+                program = self.evaluate(child)
+            
+            return program
+
+        if node.value == NT.STATEMENT:
+            sProgram = ""
+
+            for child in node.parameters:
+                line = self.evaluate(child)
+                if line:
+                    sProgram += "\n" + line
+            
+            return sProgram
+
+        if node.value == NT.BLOCK:
+            pass
+
+        if node.value in {NT.IDENTIFIER, NT.INT_LITERAL, NT.STRING_LITERAL, NT.BOOL_LITERAL}:
+            return node.parameters[0]
+
+        if node.value == NT.DECLARATION:
+            id = self.evaluate(node.parameters[1])
+            val = self.evaluate(node.parameters[2])
+            type = node.parameters[0].value
+            self.symbol_table.bind(id, val, type)
+
+        if node.value == NT.ASSIGNMENT:
+            pass
+
+        if node.value == NT.PRINT:
+            if node.parameters[0].value == NT.IDENTIFIER:
+                return "print(\"" + str(self.symbol_table.lookup(self.evaluate(node.parameters[0]))) + "\")"
+                
+            return "print(\"" + str(self.evaluate(node.parameters[0])) + "\")"
+
+        if node.value == NT.INPUT:
+            pass
+
+        if node.value == NT.FOR:
+            pass
+
+        if node.value == NT.WHILE:
+            pass
+
+        if node.value == NT.IF:
+            pass
+
+        if node.value == NT.NEGATE:
+            pass
+
+        if node.value == NT.ADD:
+            return self.evaluate(node.parameters[0]) + self.evaluate(node.parameters[1])
+
+        if node.value == NT.SUBTRACT:
+            pass
+
+        if node.value == NT.MULTIPLY:
+            pass
+
+        if node.value == NT.DIVIDE:
+            pass
+
+        if node.value == NT.EQUAL:
+            pass
+
+        if node.value == NT.AND:
+            pass
+
+        if node.value == NT.OR:
+            pass
+
+        if node.value == NT.NOT:
+            pass
+
+        if node.value == NT.DOUBLE_EQUAL:
+            pass
+
+        if node.value == NT.NOT_EQUAL:
+            pass
+
+        if node.value == NT.LESS:
+            pass
+
+        if node.value == NT.MORE:
+            pass
+
+        if node.value == NT.LESS_EQUAL:
+            pass
+
+        if node.value == NT.MORE_EQUAL:
+            pass
--- test_evaluator.py
+++ test_evaluator.py
@@ -2,19 +2,68 @@ from TreeEvaluator.TreeEvaluator import TreeEvaluator
 from Lib.Token import *
 from Lib.Token import TokenType as TT
 from Lib.Node import Node, NodeType as NT, ExpressionType as ET
-
+"""
+# print("Hello world!");
+tree = Node(NT.PROGRAM, [
+    Node(NT.STATEMENT, [
+        Node(NT.PRINT, [
+            Node(NT.STRING_LITERAL, ["Hello world!"])
+        ]),
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.STRING_LITERAL, ["How are you?"])
+            ])
+        ])
+    ])
+])
+"""
 # int a = 1 + 2;
+# print(a);
 tree = Node(NT.PROGRAM, [
     Node(NT.STATEMENT, [
         Node(NT.DECLARATION, [
-            Node(NT.IDENTIFIER, [Node(NT.STRING_LITERAL, [Node("a", [])])]),
+            Node(NT.INT_DATA_TYPE, []),
+            Node(NT.IDENTIFIER, ["a"]),
             Node(NT.ADD, [
-                Node(NT.INT_LITERAL, [Node(1, [])]),
-                Node(NT.INT_LITERAL, [Node(2, [])])
+                Node(NT.INT_LITERAL, [1]),
+                Node(NT.INT_LITERAL, [2])
             ])
-        ], ET.INT)
+        ], ET.INT),
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.IDENTIFIER, ["a"])
+            ])
+        ])
     ])
 ])
+"""
+# for (i = 0; i < 10; i++) { <body> }
+tree = Node(NT.PROGRAM, [
+    Node(NT.STATEMENT, [
+        Node(NT.FOR, [
+            Node(NT.DECLARATION, [
+                Node(NT.INT_DATA_TYPE, []),
+                Node(NT.IDENTIFIER, ["i"]),
+                Node(NT.INT_LITERAL, [0])
+            ], ET.INT),
+            Node(NT.LESS, [
+                Node(NT.IDENTIFIER, ["i"]),
+                Node(NT.INT_LITERAL, [10])
+            ]),
+            Node(NT.ASSIGNMENT, [
+                Node(NT.IDENTIFIER, ["i"]),
+                Node(NT.ADD, [
+                    Node(NT.IDENTIFIER, ["i"]),
+                    Node(NT.INT_LITERAL, [1])
+                ])
+            ]),
+            Node(NT.BLOCK, [
+                # <body>
+            ])
+        ])
+    ])
+])
+"""
 
 evaluator = TreeEvaluator()
 evaluator.run(tree)
