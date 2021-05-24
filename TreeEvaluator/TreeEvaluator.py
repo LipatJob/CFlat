161607e40d9aa8c9b7d43882e5d4eda7573fc739
charmaine <charmaine.rabano@gmail.com>
2021-05-24 20:36:37 +0800

    completed evaluator

--- TreeEvaluator/TreeEvaluator.py
+++ TreeEvaluator/TreeEvaluator.py
@@ -3,9 +3,11 @@ from Lib.Node import Node, NodeType as NT
 class SymbolTable:
     table = []
 
+    # Add new item to symbol table
     def bind(self, identifier, value, data_type):
         self.table.append([identifier, value, data_type])
 
+    # Return value of an identifier
     def lookup(self, identifier):
         for item in self.table:
             if item[0] == identifier:
@@ -13,6 +15,7 @@ class SymbolTable:
         else:
             return False
 
+    # Change value of an identifier
     def assign(self, identifier, value):
         for item in self.table:
             if item[0] == identifier:
@@ -24,82 +27,63 @@ class TreeEvaluator:
 
     def run(self, root: Node):
         if root:
-            program = self.evaluate(root)
-            exec(program)
-
-        return
+            self.evaluate(root)
     
-    def evaluate(self, node, returnStatement = False):
-        if node.value == NT.PROGRAM:
-            program = ""
-
+    # Recursive function
+    def evaluate(self, node):
+        if node.value in {NT.PROGRAM, NT.STATEMENT, NT.BLOCK}:
             for child in node.parameters:
-                program = self.evaluate(child)
-            
-            return program
+                self.evaluate(child)
 
-        if node.value in {NT.STATEMENT, NT.BLOCK}:
-            sProgram = ""
+        ## VALUES
 
-            for child in node.parameters:
-                line = self.evaluate(child)
-                if line:
-                    sProgram += "\n" + line
-            
-            return sProgram
-
-        if node.value == NT.IDENTIFIER:
-            if returnStatement:
-                return node.parameters[0]
-
-            return self.symbol_table.lookup(node.parameters[0])
-
-        if node.value in {NT.INT_LITERAL, NT.BOOL_LITERAL}:
+        if node.value in {NT.INT_LITERAL, NT.STRING_LITERAL, NT.BOOL_LITERAL}:
             return node.parameters[0]
 
-        if node.value == NT.STRING_LITERAL:
-            return "\"" + node.parameters[0] + "\""
+        if node.value == NT.IDENTIFIER:
+            return self.symbol_table.lookup(node.parameters[0])
+
+        if node.value == NT.INPUT:
+            return input()
+
+        ## STATEMENTS
 
         if node.value == NT.DECLARATION:
-            id = self.evaluate(node.parameters[1], True)
+            id = node.parameters[1].parameters[0]
             val = self.evaluate(node.parameters[2])
             type = node.parameters[0].value
             self.symbol_table.bind(id, val, type)
 
         if node.value == NT.ASSIGNMENT:
-            id = self.evaluate(node.parameters[0], True)
+            id = node.parameters[0].parameters[0]
             val = self.evaluate(node.parameters[1])
             self.symbol_table.assign(id, val)
 
         if node.value == NT.PRINT:
-            return "print(" + str(self.evaluate(node.parameters[0])) + ")"
-
-        if node.value == NT.INPUT: # NOT DONE
-            return "input()"
+            print(self.evaluate(node.parameters[0]))
 
         if node.value == NT.WHILE:
-            whileStatement = ""
             while self.evaluate(node.parameters[0]):
-                whileStatement += "\n" + self.evaluate(node.parameters[1])
-            return whileStatement
+                self.evaluate(node.parameters[1])
 
         if node.value == NT.FOR:
-            forStatement = ""
             self.evaluate(node.parameters[0])
             while self.evaluate(node.parameters[1]):
-                forStatement += "\n" + self.evaluate(node.parameters[3])
+                self.evaluate(node.parameters[3])
                 self.evaluate(node.parameters[2])
-            return forStatement
 
         if node.value in {NT.IF, NT.ELIF}:
             if (self.evaluate(node.parameters[0])):
-                return self.evaluate(node.parameters[1])
+                self.evaluate(node.parameters[1])
+                return
             
             if node.parameters[2]:
-                return self.evaluate(node.parameters[2])
+                self.evaluate(node.parameters[2])
 
         if node.value == NT.ELSE:
-            return self.evaluate(node.parameters[0])
+            self.evaluate(node.parameters[0])
+
+        ## OPERATORS
 
         if node.value == NT.NEGATE:
             return - self.evaluate(node.parameters[0])
--- test_evaluator.py
+++ test_evaluator.py
@@ -1,12 +1,14 @@
 from TreeEvaluator.TreeEvaluator import TreeEvaluator
-from Lib.Node import Node, NodeType as NT
+from Lib.Node import Node, NodeType as NT, ExpressionType as ET
 """
 # print("Hello world!");
 # print("How are you?");
 tree = Node(NT.PROGRAM, [
-    Node(NT.STATEMENT, [
-        Node(NT.PRINT, [
-            Node(NT.STRING_LITERAL, ["Hello world!"])
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.STRING_LITERAL, ["Hello world!"])
+            ])
         ]),
         Node(NT.STATEMENT, [
             Node(NT.PRINT, [
@@ -20,15 +22,17 @@ tree = Node(NT.PROGRAM, [
 # set a = a + 3;
 # print(a);
 tree = Node(NT.PROGRAM, [
-    Node(NT.STATEMENT, [
-        Node(NT.DECLARATION, [
-            Node(NT.INT_DATA_TYPE, []),
-            Node(NT.IDENTIFIER, ["a"]),
-            Node(NT.SUBTRACT, [
-                Node(NT.INT_LITERAL, [2]),
-                Node(NT.INT_LITERAL, [1])
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.DECLARATION, [
+                Node(NT.INT_DATA_TYPE, []),
+                Node(NT.IDENTIFIER, ["a"]),
+                Node(NT.SUBTRACT, [
+                    Node(NT.INT_LITERAL, [2]),
+                    Node(NT.INT_LITERAL, [1])
+                ])
             ])
-        ], ET.INT),
+        ]),
         Node(NT.STATEMENT, [
             Node(NT.ASSIGNMENT, [
                 Node(NT.IDENTIFIER, ["a"]),
@@ -36,40 +40,42 @@ tree = Node(NT.PROGRAM, [
                     Node(NT.IDENTIFIER, ["a"]),
                     Node(NT.INT_LITERAL, [3])
                 ])
-            ]),
-            Node(NT.STATEMENT, [
-                Node(NT.PRINT, [
-                    Node(NT.IDENTIFIER, ["a"])
-                ])
+            ])
+        ]), 
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.IDENTIFIER, ["a"])
             ])
         ])
     ])
 ])
 
-# for (i = 0; i < 10; set i = i + 1) { print(i); }
+# for (int i = 0; i < 10; set i = i + 1) { print(i); }
 tree = Node(NT.PROGRAM, [
-    Node(NT.STATEMENT, [
-        Node(NT.FOR, [
-            Node(NT.DECLARATION, [
-                Node(NT.INT_DATA_TYPE, []),
-                Node(NT.IDENTIFIER, ["i"]),
-                Node(NT.INT_LITERAL, [0])
-            ], ET.INT),
-            Node(NT.LESS, [
-                Node(NT.IDENTIFIER, ["i"]),
-                Node(NT.INT_LITERAL, [10])
-            ]),
-            Node(NT.ASSIGNMENT, [
-                Node(NT.IDENTIFIER, ["i"]),
-                Node(NT.ADD, [
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.FOR, [
+                Node(NT.DECLARATION, [
+                    Node(NT.INT_DATA_TYPE, []),
                     Node(NT.IDENTIFIER, ["i"]),
-                    Node(NT.INT_LITERAL, [1])
-                ])
-            ]),
-            Node(NT.BLOCK, [
-                Node(NT.STATEMENT, [
-                    Node(NT.PRINT, [
-                        Node(NT.IDENTIFIER, ["i"])
+                    Node(NT.INT_LITERAL, [0])
+                ], ET.INT),
+                Node(NT.LESS, [
+                    Node(NT.IDENTIFIER, ["i"]),
+                    Node(NT.INT_LITERAL, [10])
+                ]),
+                Node(NT.ASSIGNMENT, [
+                    Node(NT.IDENTIFIER, ["i"]),
+                    Node(NT.ADD, [
+                        Node(NT.IDENTIFIER, ["i"]),
+                        Node(NT.INT_LITERAL, [1])
+                    ])
+                ]),
+                Node(NT.BLOCK, [
+                    Node(NT.STATEMENT, [
+                        Node(NT.PRINT, [
+                            Node(NT.IDENTIFIER, ["i"])
+                        ])
                     ])
                 ])
             ])
@@ -83,12 +89,14 @@ tree = Node(NT.PROGRAM, [
 # set i = i + 1;
 # }
 tree = Node(NT.PROGRAM, [
-    Node(NT.STATEMENT, [
-        Node(NT.DECLARATION, [
-            Node(NT.INT_DATA_TYPE, []),
-            Node(NT.IDENTIFIER, ["i"]), 
-            Node(NT.INT_LITERAL, [0])
-        ], ET.INT),
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.DECLARATION, [
+                Node(NT.INT_DATA_TYPE, []),
+                Node(NT.IDENTIFIER, ["i"]),
+                Node(NT.INT_LITERAL, [0])
+            ])
+        ]), 
         Node(NT.STATEMENT, [
             Node(NT.WHILE, [
                 Node(NT.LESS, [
@@ -97,14 +105,50 @@ tree = Node(NT.PROGRAM, [
                 ]),
                 Node(NT.BLOCK, [
                     Node(NT.STATEMENT, [
-                        Node(NT.PRINT, [Node(NT.IDENTIFIER, ["i"])]),
-                        Node(NT.STATEMENT, [
-                            Node(NT.ASSIGNMENT, [
+                        Node(NT.PRINT, [Node(NT.IDENTIFIER, ["i"])])
+                    ]), 
+                    Node(NT.STATEMENT, [
+                        Node(NT.ASSIGNMENT, [
+                            Node(NT.IDENTIFIER, ["i"]),
+                            Node(NT.ADD, [
                                 Node(NT.IDENTIFIER, ["i"]),
-                                Node(NT.ADD, [
-                                    Node(NT.IDENTIFIER, ["i"]),
-                                    Node(NT.INT_LITERAL, [1])
-                                ])
+                                Node(NT.INT_LITERAL, [1])
+                            ])
+                        ])
+                    ])
+                ])
+            ])
+        ])
+    ])
+])
+
+# if (10 == 10) { print(1); }
+# elif (True) { print(2); }
+# else { print(3); }
+tree = Node(NT.PROGRAM, [
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.IF, [
+                Node(NT.DOUBLE_EQUAL, [
+                    Node(NT.INT_LITERAL, [10]),
+                    Node(NT.INT_LITERAL, [10])
+                ]),
+                Node(NT.BLOCK, [
+                    Node(NT.STATEMENT, [
+                        Node(NT.PRINT, [Node(NT.INT_LITERAL, [1])])
+                    ])
+                ]),
+                Node(NT.ELIF, [
+                    Node(NT.BOOL_LITERAL, [True]),
+                    Node(NT.BLOCK, [
+                        Node(NT.STATEMENT, [
+                            Node(NT.PRINT, [Node(NT.INT_LITERAL, [2])])
+                        ])
+                    ]),
+                    Node(NT.ELSE, [
+                        Node(NT.BLOCK, [
+                            Node(NT.STATEMENT, [
+                                Node(NT.PRINT, [Node(NT.INT_LITERAL, [3])])
                             ])
                         ])
                     ])
@@ -114,35 +158,26 @@ tree = Node(NT.PROGRAM, [
     ])
 ])
 """
-# if (10 == 10) { print(1); }
-# elif (True) { print(2); }
-# else { print(3); }
+# print("Enter a number:");
+# int x = input();
+# print(x);
 tree = Node(NT.PROGRAM, [
-    Node(NT.STATEMENT, [
-        Node(NT.IF, [
-            Node(NT.DOUBLE_EQUAL, [
-                Node(NT.INT_LITERAL, [10]),
-                Node(NT.INT_LITERAL, [10])
-            ]),
-            Node(NT.BLOCK, [
-                Node(NT.STATEMENT, [
-                    Node(NT.PRINT, [Node(NT.INT_LITERAL, [1])])
-                ])
-            ]),
-            Node(NT.ELIF, [
-                Node(NT.BOOL_LITERAL, [True]),
-                Node(NT.BLOCK, [
-                    Node(NT.STATEMENT, [
-                        Node(NT.PRINT, [Node(NT.INT_LITERAL, [2])])
-                    ])
-                ]),
-                Node(NT.ELSE, [
-                    Node(NT.BLOCK, [
-                        Node(NT.STATEMENT, [
-                            Node(NT.PRINT, [Node(NT.INT_LITERAL, [3])])
-                        ])
-                    ])
-                ])
+    Node(NT.BLOCK, [
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.STRING_LITERAL, ["Enter a number:"])
+            ])
+        ]),
+        Node(NT.STATEMENT, [
+            Node(NT.DECLARATION, [
+                Node(NT.INT_DATA_TYPE, []),
+                Node(NT.IDENTIFIER, ["x"]),
+                Node(NT.INPUT, [])
+            ])
+        ]),
+        Node(NT.STATEMENT, [
+            Node(NT.PRINT, [
+                Node(NT.IDENTIFIER, ["x"])
             ])
         ])
     ])
