ff72ab957a1a0c3e253893fc1ab0e1e690e2b777
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-26 19:26:02 +0800

    implemented testcaseparser

--- TestCases.py
+++ TestCases.py
@@ -5,7 +5,7 @@ from LexicalAnalyzer.LexicalAnalyzer import LexicalAnalyzer
 from SemanticAnalyzer.SemanticAnalyzer import  SemanticAnalyzer
 from SyntaxAnalyzer.SyntaxAnalyzer import SyntaxAnalyzer
 from TreeEvaluator.TreeEvaluator import TreeEvaluator
-
+import Lib.TestCaseParser as InOut
 def run_compiler(file_name):
         lexer = LexicalAnalyzer()
         parser = SyntaxAnalyzer()
@@ -22,11 +22,9 @@ def run_compiler(file_name):
 class TestSyntaxAnalayzer(unittest.TestCase):
     @patch("builtins.input")
     def test1(self, mocked_input):
-        test_inputs = [
-        ]
+        with open("Tests/SyntaxAnalyzer/input_output.txt") as f:
+            filenames, test_inputs, expected_outputs = InOut.parse(f.read())
 
-        filenames = glob.glob("Tests/SyntaxAnalyzer/testcase*.cf")
-        filenames.sort()
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
@@ -35,11 +33,9 @@ class TestSyntaxAnalayzer(unittest.TestCase):
 class TestSemanticAnalyzer(unittest.TestCase):
     @patch("builtins.input")
     def test1(self, mocked_input):
-        test_inputs = [
-        ]
+        with open("Tests/SemanticAnalyzer/input_output.txt") as f:
+            filenames, test_inputs, expected_outputs = InOut.parse(f.read())
 
-        filenames = glob.glob("Tests/SemanticAnalyzer/testcase*.cf")
-        filenames.sort()
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
@@ -48,11 +44,9 @@ class TestSemanticAnalyzer(unittest.TestCase):
 class TestLexicalAnalyzer(unittest.TestCase):
     @patch("builtins.input")
     def test1(self, mocked_input):
-        test_inputs = [
-        ]
+        with open("Tests/LexicalAnalyzer/input_output.txt") as f:
+            filenames, test_inputs, expected_outputs = InOut.parse(f.read())
 
-        filenames = glob.glob("Tests/LexicalAnalyzer/testcase*.cf")
-        filenames.sort()
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
@@ -63,15 +57,8 @@ class TestWorking(unittest.TestCase):
 
     @patch("builtins.input")
     def test1(self, mocked_input):
-        test_inputs = [
-        ]
-
-        expected_outputs = [
-            '"Hello world!"\n"Have a good day!"\n'
-        ]
-
-        filenames = glob.glob("Tests/Working/testcase*.cf")
-        filenames.sort()
+        with open("Tests/Working/input_output.txt") as f:
+            filenames, test_inputs, expected_outputs = InOut.parse(f.read())
         for test_input, expected_output, filename in zip(test_inputs, expected_outputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
