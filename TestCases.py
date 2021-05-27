b93bc20ad0b731eba430e0d34022da39fd339114
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-27 22:51:13 +0800

    cleaned git ignore

--- TestCases.py
+++ TestCases.py
@@ -1,23 +1,25 @@
 import glob
 import unittest
-from unittest.mock import patch 
+from unittest.mock import patch
 from LexicalAnalyzer.LexicalAnalyzer import LexicalAnalyzer
-from SemanticAnalyzer.SemanticAnalyzer import  SemanticAnalyzer
+from SemanticAnalyzer.SemanticAnalyzer import SemanticAnalyzer
 from SyntaxAnalyzer.SyntaxAnalyzer import SyntaxAnalyzer
 from TreeEvaluator.TreeEvaluator import TreeEvaluator
 import Lib.TestCaseParser as InOut
+
+
 def run_compiler(file_name):
-        lexer = LexicalAnalyzer()
-        parser = SyntaxAnalyzer()
-        semanticAnalyzer = SemanticAnalyzer()
-        evaluator = TreeEvaluator()
+    lexer = LexicalAnalyzer()
+    parser = SyntaxAnalyzer()
+    semanticAnalyzer = SemanticAnalyzer()
+    evaluator = TreeEvaluator()
 
+    tokens = lexer.run(file_name)
+    tree = parser.run(tokens)
 
-        tokens = lexer.run(file_name)
-        tree = parser.run(tokens)
+    semanticAnalyzer.run(tree)
+    return evaluator.run(tree)
 
-        semanticAnalyzer.run(tree)
-        return evaluator.run(tree)
 
 class TestSyntaxAnalayzer(unittest.TestCase):
     @patch("builtins.input")
@@ -28,8 +30,10 @@ class TestSyntaxAnalayzer(unittest.TestCase):
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
-                self.assertRaises(Exception, run_compiler, "Tests/SyntaxAnalyzer/"+filename)
+                self.assertRaises(Exception, run_compiler,
+                                  "Tests/SyntaxAnalyzer/"+filename)
         print(f"Syntax Analyzer: Executed {len(filenames)} test cases")
+
 
 class TestSemanticAnalyzer(unittest.TestCase):
     @patch("builtins.input")
@@ -40,8 +44,10 @@ class TestSemanticAnalyzer(unittest.TestCase):
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
-                self.assertRaises(Exception, run_compiler, "Tests/SemanticAnalyzer/"+filename)
+                self.assertRaises(Exception, run_compiler,
+                                  "Tests/SemanticAnalyzer/"+filename)
         print(f"Semantic Analyzer: Executed {len(filenames)} test cases")
+
 
 class TestLexicalAnalyzer(unittest.TestCase):
     @patch("builtins.input")
@@ -52,8 +58,10 @@ class TestLexicalAnalyzer(unittest.TestCase):
         for test_input, filename in zip(test_inputs, filenames):
             with self.subTest(filename=filename):
                 mocked_input.side_effect = test_input
-                self.assertRaises(Exception, run_compiler, "Tests/LexicalAnalyzer/"+filename)
+                self.assertRaises(Exception, run_compiler,
+                                  "Tests/LexicalAnalyzer/"+filename)
         print(f"Lexical Analyzer: Executed {len(filenames)} test cases")
+
 
 class TestWorking(unittest.TestCase):
     @patch("builtins.input")
@@ -63,10 +71,11 @@ class TestWorking(unittest.TestCase):
 
         for test_input, expected_output, filename in zip(test_inputs, expected_outputs, filenames):
             with self.subTest(filename=filename):
-                mocked_input.side_effect=test_input
+                mocked_input.side_effect = test_input
 
                 actual = run_compiler("Tests/Working/"+filename)
                 self.assertEquals(expected_output, actual)
         print(f"Working: Executed {len(filenames)} test cases")
-        
+
+
 unittest.main()
