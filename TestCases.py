16bbb90dd457b11d2105dc46a34e597866de8d40
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-06-05 08:55:53 +0800

    unskipped test cases

--- TestCases.py
+++ TestCases.py
@@ -137,7 +137,7 @@ class CompilerTestCase(unittest.TestCase):
         OutputFormatter.display_evaluated_symbol_table(symbol_table)
 
 class TestSyntaxAnalayzer(CompilerTestCase):
-    @skip("Test case disabled")
+    #@skip("Test case disabled")
     @patch("builtins.input")
     def test_syntax_analyzer(self, mocked_input):
         # Get all test case files
@@ -164,7 +164,7 @@ class TestSemanticAnalyzer(CompilerTestCase):
 
 
 class TestLexicalAnalyzer(CompilerTestCase):
-    @skip("Test case disabled")
+    # @skip("Test case disabled")
     @patch("builtins.input")
     def test_lexical_analyzer(self, mocked_input):
         filenames = glob.glob(
